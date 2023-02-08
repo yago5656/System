ed59592d83e138300a523f0602c50eb466f80cf6
yago5656 <68473807+yago5656@users.noreply.github.com>
2023-02-08 20:47:24 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -58,7 +58,7 @@ scanner.addListener("scan", function(content) {
 });
 Instascan.Camera.getCameras().then(function(cameras) {
   if (cameras.length > 0) {
-    scanner.start(cameras[0]);
+    scanner.start(cameras[1]);
   } else {
     console.error("Nenhuma câmera encontrada.");
   }
